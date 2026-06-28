df6109ab1268d1ca32e13348c25b10dcf748b134
Vineeth Krishnan <vineethkrishnan1812@gmail.com>
2026-06-28 13:41:24 +0530

    Add project introduction logs to script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+console.log("================================");
+console.log("      PROJECT MAYA v0.1");
+console.log("      THE BEGINNING");
+console.log("================================");
 
+window.onload = () => {
+
+    setTimeout(() => {
+
+        document.getElementById("loading").style.display = "none";
+
+    },2000);
+
+};
